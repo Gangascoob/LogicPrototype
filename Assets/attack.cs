ffcb1dfa83fe2eb415bb0cc16a392dc30564cf45
Gangascoob <greg.scobie@gmail.com>
2023-03-09 18:16:34 +0000

    some progress

---
+++ Assets/attack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            animator.Play("slash");
+        }
+    }
+}
